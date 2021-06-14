185b9768f29ca5c6c00765a8dbf3cb56f18963cd
unknown <hoanganblue@gmail.com>
2021-06-14 21:49:24 +0700

    update :)

--- scripts/munit/edges2shoes-r_fast/export.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-python export.py \
-  --input_path supernet-2000000/checkpoints/latest_net_G.pth \
-  --output_path pretrained/munit/edges2shoes-r/compressed/latest_net_G.pth \
-  --ngf 64 --model mobile_munit --config_str $1
--- train_supernet.py
+++
@@ -1,6 +0,0 @@
-from trainer import Trainer
-
-if __name__ == '__main__':
-    trainer = Trainer('supernet')
-    trainer.start()
-    print('Supernet training finished!!!')
